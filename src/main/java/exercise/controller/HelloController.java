06abc0ae08b123204dc509e3e17f9b7628da06ab
viper12590 <viper1259@gmail.com>
2020-09-16 15:03:43 +0100

    print hello world at /hello

--- src/main/java/exercise/controller/HelloController.java
+++ src/main/java/exercise/controller/HelloController.java
@@ -1,7 +1,15 @@
 package exercise.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "hello world";
+    }
 }
