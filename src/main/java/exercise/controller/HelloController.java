1888967e8aab8d943b59b8c5a320ac52ee1979a4
viper12590 <viper1259@gmail.com>
2020-09-16 15:11:53 +0100

    add name attribute to model, use Thymeleaf to parse attribute

--- src/main/java/exercise/controller/HelloController.java
+++ src/main/java/exercise/controller/HelloController.java
@@ -1,15 +1,16 @@
 package exercise.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String helloWorld(@RequestParam(defaultValue = "world", required = false) String name) {
+    public String helloWorld(@RequestParam(defaultValue = "world", required = false) String name, Model model) {
+        model.addAttribute("name",name);
         return "hello";
     }
 }
