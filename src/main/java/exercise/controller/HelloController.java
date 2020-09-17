f2ff35819e49b56529065f616d269c28105b3ada
viper12590 <viper1259@gmail.com>
2020-09-17 10:31:07 +0100

    Testing example

--- src/main/java/exercise/controller/HelloController.java
+++ src/main/java/exercise/controller/HelloController.java
@@ -4,13 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
+
+    public List<String> names = new ArrayList<>();
 
     @GetMapping("/hello")
     public String helloWorld(@RequestParam(defaultValue = "world", required = false) String name, Model model) {
         model.addAttribute("name",name);
         return "hello";
+    }
+
+    @GetMapping("/addName")
+    @ResponseBody
+    public String addName(@RequestParam(required = false) String name) {
+        if(name != null) {
+            names.add(name);
+            return "Success";
+        }
+
+        return "Failed";
     }
 }
--- src/test/java/exercise/ExerciseApplicationTests.java
+++ src/test/java/exercise/ExerciseApplicationTests.java
@@ -1,13 +1,25 @@
 package exercise;
 
+import exercise.controller.HelloController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExerciseApplicationTests {
 
+	@Autowired
+	HelloController helloController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void addTest() {
+		assert(helloController.addName(null).equals("Failed"));
+		assert(helloController.addName("test").equals("Success"));
+		assert(helloController.names.contains("test"));
 	}
 
 }
